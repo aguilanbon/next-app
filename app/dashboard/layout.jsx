5aad3d5a4fbd6ec9e51b0cc41f085804bec14898
Bon Aguilan <aguilanbon@gmail.com>
2023-02-26 17:58:35 +0800

    added temp profile for nav, added unsplash domain to next config

--- app/dashboard/layout.jsx
+++ app/dashboard/layout.jsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 export default function DashboardLayout({ children }) {
   return (
     <div>
@@ -22,22 +24,33 @@ export default function DashboardLayout({ children }) {
         <div className="flex-1">
           <a className="btn btn-ghost normal-case text-xl">Dashboard</a>
         </div>
-        <div className="flex-none">
-          <button className="btn btn-square btn-ghost">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              className="inline-block w-5 h-5 stroke-current"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"
-              ></path>
-            </svg>
-          </button>
+        <div className="dropdown dropdown-end">
+          <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+            <div className="w-10 rounded-full">
+              <Image
+                width={200}
+                height={200}
+                src={`https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80`}
+              ></Image>
+            </div>
+          </label>
+          <ul
+            tabIndex={0}
+            className="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52"
+          >
+            <li>
+              <a className="justify-between">
+                Profile
+                <span className="badge">New</span>
+              </a>
+            </li>
+            <li>
+              <a>Settings</a>
+            </li>
+            <li>
+              <a>Logout</a>
+            </li>
+          </ul>
         </div>
       </div>
       <div className="bg-base-200 min-h-screen">{children}</div>
