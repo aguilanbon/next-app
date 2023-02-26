0ff14534fc14bd28bbc2fc2f1bc800fc45753654
Bon Aguilan <aguilanbon@gmail.com>
2023-02-26 18:30:18 +0800

    added recharts, initial chart ui

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,3 +1,4 @@
+import DashboardLineChart from "./LineChart";
 import DashboardStats from "./Stats";
 import DashboardTable from "./Table";
 
@@ -5,6 +6,9 @@ export default function Dashboard() {
   return (
     <div className="w-full flex">
       <div className="w-full flex flex-col items-center justify-center">
+        <div className="w-full h-1/2">
+          <DashboardLineChart />
+        </div>
         <DashboardStats />
         <DashboardTable />
       </div>
